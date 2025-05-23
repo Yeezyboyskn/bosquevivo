2d74fe91ea763bd0e5cd43a4bbadf8b980e642b2
yeezyboyskn <ofariasb@correo.uss.cl>
2025-05-23 19:11:15 -0400

    Primera versión del sitio BosqueVivo

---
+++ script.js
@@ -0,0 +1,10 @@
+function showSection(id) {
+  document.querySelectorAll('.section').forEach(section => {
+    section.classList.remove('active');
+  });
+  document.getElementById(id).classList.add('active');
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  showSection('inicio');
+});
